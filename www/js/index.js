8852a11bc98a68a417f063506a289f30c8e24f3a
Connor Clarkson <clarksoc@sheridancollege.ca>
2019-04-17 13:56:42 -0400

    Final Code Cleanup; Ready for Submitting!

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,12 @@
 let app = {
-    // Application Constructor
     initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
-    // Update DOM on a Received Event
     receivedEvent: function (id) {
         let parentElement = document.getElementById(id);
         let listeningElement = parentElement.querySelector('.listening');
